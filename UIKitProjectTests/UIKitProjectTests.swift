e5fe2739337eb917e4c88f7ff32ad9c0d35a39aa
gayoung <rkdudwydkv@gmail.com>
2024-11-13 16:15:47 +0900

    Initial Commit

---
+++ UIKitProjectTests/UIKitProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitProjectTests.swift
+//  UIKitProjectTests
+//
+//  Created by Gayoung Kim on 11/13/24.
+//
+
+import Testing
+@testable import UIKitProject
+
+struct UIKitProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
